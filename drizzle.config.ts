c0e912d35c454e83641eb3fcad0c0630b5892b56
kalil0321 <kalil.bouzigues@gmail.com>
2025-05-24 21:48:43 +0200

    install drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env.local file
+dotenv.config({ path: ".env" });
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/lib/drizzle/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
